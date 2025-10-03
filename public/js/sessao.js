8c30c646c7c8e876e2e022913e0847fd1ad10232
GiovannePDS7 <giovanne3282@gmail.com>
2025-10-03 11:24:59 -0300

    add: desenvolvendo front cadastro

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -1,17 +1,17 @@
-function validarSessao() {
-  var email = sessionStorage.EMAIL_USUARIO;
-  var nome = sessionStorage.NOME_USUARIO;
+// function validarSessao() {
+//   var email = sessionStorage.EMAIL_USUARIO;
+//   var nome = sessionStorage.NOME_USUARIO;
 
-  var b_usuario = document.getElementById("b_usuario");
+//   var b_usuario = document.getElementById("b_usuario");
 
-  if (email != null && nome != null) {
-    b_usuario.innerHTML = nome;
-  } else {
-    window.location = "../login.html";
-  }
-}
+//   if (email != null && nome != null) {
+//     b_usuario.innerHTML = nome;
+//   } else {
+//     window.location = "../login.html";
+//   }
+// }
 
-function limparSessao() {
-  sessionStorage.clear();
-  window.location = "../login.html";
-}
+// function limparSessao() {
+//   sessionStorage.clear();
+//   window.location = "../login.html";
+// }
