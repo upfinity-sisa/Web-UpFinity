c54261a87ef5850ec8d0d1d08036cdaad345e52b
brenokas <brenofreitas28f@gmail.com>
2025-11-14 09:20:54 -0300

    add: adicionando atualização das kpis ao entrar na dashboard pela primeira vez

--- public/Pages/dashboard/main.js
+++ public/Pages/dashboard/main.js
@@ -1,18 +1,13 @@
+let dadosCpu = []
+let dadosRam = []
+let dadosDisco = []
+let dadosRede = []
+
 function ultimasCapturas() {
   fetch(`/dashboard/ultimas-capturas/1`, { cache: 'no-store' }).then(
     (response) => {
       if (response.ok) {
         response.json().then((resposta) => {
-          // 1 - CPU 
-          // 2 - RAM
-          // 3 - Disco
-          // 4 - Rede
-
-          let dadosCpu = []
-          let dadosRam = []
-          let dadosDisco = []
-          let dadosRede = []
-
           for (let i = 0; i < resposta.length; i++){
             switch (resposta[i]["fkComponente"]) {
               case 1:
@@ -30,6 +25,21 @@ function ultimasCapturas() {
               default:
                 break;
             }
+          }
+
+          let kpiCpu = document.getElementById("dado-kpi-cpu");
+          let kpiRam = document.getElementById("dado-kpi-ram");
+          let kpiDisco = document.getElementById("dado-kpi-disco");
+          let kpiRede = document.getElementById("dado-kpi-rede");
+
+          kpiCpu.innerHTML  = dadosCpu[dadosCpu.length-1] + "%"
+          kpiRam.innerHTML  = dadosRam[dadosRam.length-1] + "%"
+          kpiDisco.innerHTML  = dadosDisco[dadosDisco.length-1] + "%"
+
+          if (dadosRede[dadosRede.length-1] > 0) {
+            kpiRede.innerHTML = "Conectado"
+          } else {
+            kpiRede.innerHTML = "Não conectado"
           }
         })
       } else {
