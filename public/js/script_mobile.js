53d4c527a19641d90a393782c5fe7e3f2a13c2ac
GiovannePDS7 <giovanne3282@gmail.com>
2025-11-16 19:11:56 -0300

    add: deixando a navegação do site dinâmica

--- public/js/script_mobile.js
+++ public/js/script_mobile.js
@@ -25,3 +25,65 @@ btn_header_menu_mobile.addEventListener('click', function () {
         FecharMenuMobile()
     }
 })
+
+const containerInicio_Mobile = document.getElementById('container_inicial_mobile')
+
+const aInicioHeader_Mobile = document.getElementById('aInicioHeader_Mobile')
+const aInicioFooter_Mobile = document.getElementById('aInicioFooter_Mobile')
+
+aInicioHeader_Mobile.addEventListener('click', function () {
+    containerInicio_Mobile.scrollIntoView({ block: "center", behavior: "smooth" })
+    FecharMenuMobile()
+})
+
+aInicioFooter_Mobile.addEventListener('click', function () {
+    containerInicio_Mobile.scrollIntoView({ block: "center", behavior: "smooth" })
+    FecharMenuMobile()
+})
+
+
+const containerBeneficios_Mobile = document.getElementById('container_oque_fazemos_mobile')
+
+const aBeneficioHeader_Mobile = document.getElementById('aBeneficioHeader_Mobile')
+const aBeneficiosFooter_Mobile = document.getElementById('aBeneficiosFooter_Mobile')
+
+aBeneficioHeader_Mobile.addEventListener('click', function () {
+    containerBeneficios_Mobile.scrollIntoView({ block: "start", behavior: "smooth" })
+    FecharMenuMobile()
+})
+
+aBeneficiosFooter_Mobile.addEventListener('click', function () {
+    containerBeneficios_Mobile.scrollIntoView({ block: "start", behavior: "smooth" })
+    FecharMenuMobile()
+})
+
+const containerDiferencial_Mobile = document.getElementById('container_diferencial-mobile')
+
+const aDiferencialHeader_Mobile = document.getElementById('aDiferencialHeader_Mobile')
+const aDiferencialFooter_Mobile = document.getElementById('aDiferencialFooter_Mobile')
+
+aDiferencialHeader_Mobile.addEventListener('click', function () {
+    containerDiferencial_Mobile.scrollIntoView({ block: "start", behavior: "smooth" })
+    FecharMenuMobile()
+})
+
+aDiferencialFooter_Mobile.addEventListener('click', function () {
+    containerDiferencial_Mobile.scrollIntoView({ block: "start", behavior: "smooth" })
+    FecharMenuMobile()
+})
+
+const containerFluxo_Mobile = document.getElementById('container_fluxo_mobile')
+
+const aFluxoHeader_Mobile = document.getElementById('aFluxoHeader_Mobile')
+const aFluxoFooter_Mobile = document.getElementById('aFluxoFooter_Mobile')
+
+aFluxoHeader_Mobile.addEventListener('click', function () {
+    containerFluxo_Mobile.scrollIntoView({ block: "start", behavior: "smooth" })
+    FecharMenuMobile()
+})
+
+aFluxoFooter_Mobile.addEventListener('click', function () {
+    containerFluxo_Mobile.scrollIntoView({ block: "start", behavior: "smooth" })
+    FecharMenuMobile()
+})
+
