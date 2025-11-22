2a1a346321a55f4036aa2c18dc59ccafe7a5f744
brenokas <brenofreitas28f@gmail.com>
2025-11-22 16:43:56 -0300

    refactor: refazendo todo o frontend da dashboard de cpu

--- public/Pages/dashboardsIndividuais/cpu/dashCpu.js
+++ public/Pages/dashboardsIndividuais/cpu/dashCpu.js
@@ -1,10 +1,14 @@
 let dados_uso = [];
 let horarios_uso = [];
 let dados_temperatura = [];
+let horarios_temperatura = [];
 let dados_frequencia = [];
 let graficoLinha = null;
 let graficoTemperatura = null;
 let graficoRelacao = null;
+let graficoMaioresUsos1 = null;
+let graficoMaioresUsos2 = null;
+let graficoMaioresUsos3 = null;
 let comboATMSCPU = document.getElementById('comboATMs');
 
 function vwParaPx(vwValue) {
@@ -17,6 +21,7 @@ function carregarDadosCPU(idAtm) {
     dados_uso = [];
     horarios_uso = [];
     dados_temperatura = [];
+    horarios_temperatura = [];
     dados_frequencia = [];
     fetch(`/cpu/dados/${idAtm}`, { cache: 'no-store' })
         .then(response => {
@@ -45,16 +50,17 @@ function carregarDadosCPU(idAtm) {
                         };
 
                         for (let i = 0; i < resposta.length; i++) {
-                            horarios_uso.push(formatarHorario(resposta[i].horario));
                             switch (parseInt(resposta[i].fkComponente)) {
                                 case 1:
                                     dados_uso.push(resposta[i].valor);
+                                    horarios_uso.push(formatarHorario(resposta[i].horario));
                                     break;
                                 case 5:
                                     dados_frequencia.push(resposta[i].valor);
                                     break;
                                 case 6:
                                     dados_temperatura.push(resposta[i].valor);
+                                    horarios_temperatura.push(formatarHorario(resposta[i].horario));
                                     break;
                                 default:
                                     break;
@@ -180,7 +186,7 @@ function plotarGraficoLinha() {
                 },
             ],
             chart: {
-                height: 400,
+                height: 300,
                 type: 'line',
                 zoom: {
                     enabled: false,
@@ -190,7 +196,7 @@ function plotarGraficoLinha() {
                 enabled: false,
             },
             stroke: {
-                curve: 'straight',
+                curve: 'smooth',
                 width: `${vwParaPx(0.2)}`,
             },
             title: {
@@ -291,24 +297,24 @@ function plotarGraficoTemperatura() {
                 },
             ],
             chart: {
-                height: 400,
-                type: 'line',
+                height: 300,
+                type: 'bar',
                 zoom: {
                     enabled: false,
                 },
             },
+
             dataLabels: {
                 enabled: false,
             },
             stroke: {
-                curve: 'straight',
-                width: `${vwParaPx(0.2)}`,
+                width: 0,
             },
             title: {
                 align: 'left',
             },
             xaxis: {
-                categories: horarios_uso,
+                categories: horarios_temperatura,
             },
             legend: {
                 position: 'top',
@@ -332,7 +338,16 @@ function plotarGraficoTemperatura() {
                 offsetX: 0,
                 offsetY: 0,
             },
-            colors: ['#268184'],
+            fill: {
+                type: 'gradient',
+                gradient: {
+                    shade: 'dark',
+                    type: 'vertical',
+                    shadeIntensity: 0.5,
+                    inverseColors: false,
+                    stops: [0, 100],
+                },
+            },
             yaxis: {
                 min: 0,
                 max: 100,
@@ -389,7 +404,7 @@ function plotarGraficoTemperatura() {
                 },
             ],
             xaxis: {
-                categories: horarios_uso,
+                categories: horarios_temperatura,
             },
         });
     }
@@ -409,8 +424,15 @@ function plotarGraficoRelacao() {
                 },
             ],
             chart: {
-                height: 400,
+                height: 300,
                 type: 'line',
+                animations: {
+                    enabled: true,
+                    easing: 'linear',
+                    dynamicAnimation: {
+                        speed: 1000,
+                    },
+                },
                 zoom: {
                     enabled: false,
                 },
@@ -424,7 +446,7 @@ function plotarGraficoRelacao() {
             },
             title: {
                 align: 'left',
-            },          
+            },
             xaxis: {
                 categories: horarios_uso,
             },
@@ -484,9 +506,225 @@ function plotarGraficoRelacao() {
     }
 }
 
+function plotarGraficoMaioresUsos() {
+    if (graficoMaioresUsos1 == null && graficoMaioresUsos2 == null && graficoMaioresUsos3 == null) {
+        let optionsBarra1 = {
+            chart: {
+                height: 70,
+                type: 'bar',
+                stacked: true,
+                sparkline: {
+                    enabled: true,
+                },
+            },
+            plotOptions: {
+                bar: {
+                    horizontal: true,
+                    barHeight: '20%',
+                    colors: {
+                        backgroundBarColors: ['#40475D'],
+                    },
+                },
+            },
+            stroke: {
+                width: 0,
+            },
+            series: [
+                {
+                    name: 'Process 1',
+                    data: [44],
+                },
+            ],
+            title: {
+                floating: true,
+                offsetX: -10,
+                offsetY: 5,
+                text: 'Process 1',
+            },
+            subtitle: {
+                floating: true,
+                align: 'right',
+                offsetY: 0,
+                text: '44%',
+                style: {
+                    fontSize: '20px',
+                },
+            },
+            tooltip: {
+                enabled: false,
+            },
+            xaxis: {
+                categories: ['Process 1'],
+            },
+            yaxis: {
+                max: 100,
+            },
+            fill: {
+                opacity: 1,
+            },
+        };
+
+        let optionsBarra2 = {
+            chart: {
+                height: 70,
+                type: 'bar',
+                stacked: true,
+                sparkline: {
+                    enabled: true,
+                },
+            },
+            plotOptions: {
+                bar: {
+                    horizontal: true,
+                    barHeight: '20%',
+                    colors: {
+                        backgroundBarColors: ['#40475D'],
+                    },
+                },
+            },
+            stroke: {
+                width: 0,
+            },
+            series: [
+                {
+                    name: 'Process 1',
+                    data: [44],
+                },
+            ],
+            title: {
+                floating: true,
+                offsetX: -10,
+                offsetY: 5,
+                text: 'Process 1',
+            },
+            subtitle: {
+                floating: true,
+                align: 'right',
+                offsetY: 0,
+                text: '44%',
+                style: {
+                    fontSize: '20px',
+                },
+            },
+            tooltip: {
+                enabled: false,
+            },
+            xaxis: {
+                categories: ['Process 1'],
+            },
+            yaxis: {
+                max: 100,
+            },
+            fill: {
+                opacity: 1,
+            },
+            colors: ['#6078ea'],
+        };
+
+        let optionsBarra3 = {
+            chart: {
+                height: 70,
+                type: 'bar',
+                stacked: true,
+                sparkline: {
+                    enabled: true,
+                },
+            },
+            plotOptions: {
+                bar: {
+                    horizontal: true,
+                    barHeight: '20%',
+                    colors: {
+                        backgroundBarColors: ['#40475D'],
+                    },
+                },
+            },
+            stroke: {
+                width: 0,
+            },
+            series: [
+                {
+                    name: 'Process 1',
+                    data: [44],
+                },
+            ],
+            title: {
+                floating: true,
+                offsetX: -10,
+                offsetY: 5,
+                text: 'Process 1',
+            },
+            subtitle: {
+                floating: true,
+                align: 'right',
+                offsetY: 0,
+                text: '44%',
+                style: {
+                    fontSize: '20px',
+                },
+            },
+            tooltip: {
+                enabled: false,
+            },
+            xaxis: {
+                categories: ['Process 1'],
+            },
+            yaxis: {
+                max: 100,
+            },
+            fill: {
+                opacity: 1,
+            },
+            colors: ['#e94560'],
+        };
+
+        graficoMaioresUsos1 = new ApexCharts(
+            document.getElementById('linha-grafico-1'),
+            optionsBarra1
+        );
+        graficoMaioresUsos1.render();
+        graficoMaioresUsos2 = new ApexCharts(
+            document.getElementById('linha-grafico-2'),
+            optionsBarra2
+        );
+        graficoMaioresUsos2.render();
+        graficoMaioresUsos3 = new ApexCharts(
+            document.getElementById('linha-grafico-3'),
+            optionsBarra3
+        );
+        graficoMaioresUsos3.render();
+    } else {
+        graficoMaioresUsos1.updateOptions({
+            series: [
+                {
+                    name: 'Process 1',
+                    data: [44],
+                },
+            ],
+        });
+        graficoMaioresUsos2.updateOptions({
+            series: [
+                {
+                    name: 'Process 1',
+                    data: [44],
+                },
+            ],
+        });
+        graficoMaioresUsos3.updateOptions({
+            series: [
+                {
+                    name: 'Process 1',
+                    data: [44],
+                },
+            ],
+        });
+    }
+}
+
 window.addEventListener('DOMContentLoaded', () => {
     carregarAtms();
     atualizarParametrosKpis();
+    plotarGraficoMaioresUsos();
     intervaloAtualizacao = setInterval(atualizarDadosCpu, 3500);
 
     comboATMSCPU.addEventListener('change', () => {
