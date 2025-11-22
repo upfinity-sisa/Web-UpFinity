d2607e96d860ff007f136f4738e17fecaa979bf9
brenokas <brenofreitas28f@gmail.com>
2025-11-21 23:51:05 -0300

    add: plotando o grafico com os dados do banco

--- public/Pages/dashboardsIndividuais/cpu/dashCpu.js
+++ public/Pages/dashboardsIndividuais/cpu/dashCpu.js
@@ -4,113 +4,8 @@ function vwParaPx(vwValue) {
     return Math.round(valorEmPx);
 }
 
-var options = {
-    series: [
-        {
-            name: 'Uso',
-            data: [10, 41, 35, 51, 49, 62, 69, 91, 18],
-        },
-    ],
-    chart: {
-        height: 400,
-        type: 'line',
-        zoom: {
-            enabled: false,
-        },
-    },
-    dataLabels: {
-        enabled: false,
-    },
-    stroke: {
-        curve: 'straight',
-        width: `${vwParaPx(0.2)}`,
-    },
-    title: {
-        align: 'left',
-    },
-    xaxis: {
-        categories: [
-            '11:00:00',
-            '11:00:03',
-            '11:00:06',
-            '11:00:09',
-            '11:00:12',
-            '11:00:15',
-            '11:00:18',
-            '11:00:21',
-            '11:00:24',
-            '11:00:27',
-        ],
-    },
-    legend: {
-        position: 'top',
-        fontSize: `${vwParaPx(1)}px`,
-        fontFamily: 'poppins leve',
-        markers: {
-            size: `${vwParaPx(0.4)}`,
-        },
-    },
-    toolbar: {
-        show: true,
-        tools: {
-            download: true,
-            selection: false,
-            zoom: false,
-            zoomin: false,
-            zoomout: false,
-            pan: false,
-            reset: false,
-        },
-        offsetX: 0,
-        offsetY: 0,
-    },
-    colors: ['#268184'],
-    yaxis: {
-        min: 0,
-        max: 100,
-        labels: {
-            style: {
-                fontSize: `${vwParaPx(0.8)}px`,
-                fontFamily: 'poppins leve',
-            },
-        },
-    },
-    annotations: {
-        yaxis: [
-            {
-                y: 37.83,
-                borderColor: '#FF4560',
-                strokeDashArray: 0,
-                label: {
-                    borderColor: '#FF4560',
-                    style: {
-                        color: '#fff',
-                        background: '#FF4560',
-                    },
-                    text: 'Crítico',
-                },
-            },
-            {
-                y: 31.23,
-                borderColor: '#f4a261',
-                strokeDashArray: 0,
-                label: {
-                    borderColor: '#f4a261',
-                    style: {
-                        color: '#fff',
-                        background: '#f4a261',
-                    },
-                    text: 'Importante',
-                },
-            },
-        ],
-    },
-};
-
-var chart = new ApexCharts(document.querySelector('#grafico-linha'), options);
-chart.render();
-
 let dados_uso = [];
+let horarios_uso = [];
 let dados_temperatura = [];
 let dados_frequencia = [];
 
@@ -133,10 +28,19 @@ function carregarDadosCPU(idAtm) {
 
                     resposta.reverse();
                     if (resposta.length > 0) {
+                        const formatarHorario = horario => {
+                            let data = new Date(horario);
+                            let horas = data.getHours().toString().padStart(2, '0');
+                            let minutos = data.getMinutes().toString().padStart(2, '0');
+                            let segundos = data.getSeconds().toString().padStart(2, '0');
+                            return `${horas}:${minutos}:${segundos}`;
+                        };
+
                         for (let i = 0; i < resposta.length; i++) {
                             switch (parseInt(resposta[i].fkComponente)) {
                                 case 1:
                                     dados_uso.push(resposta[i].valor);
+                                    horarios_uso.push(formatarHorario(resposta[i].horario));
                                     break;
                                 case 5:
                                     dados_frequencia.push(resposta[i].valor);
@@ -188,6 +92,8 @@ function carregarDadosCPU(idAtm) {
                             statusTemperatura.innerHTML = 'Normal';
                             statusTemperatura.style.color = corOk;
                         }
+
+                        plotarGraficoLinha();
                     }
                 });
             } else {
@@ -253,6 +159,103 @@ function atualizarParametrosKpis() {
     parametroModeradoTemperaturaDiv.innerHTML = parseFloat(
         sessionStorage.getItem('PARAM_IMPORTANTE_TEMPERATURA_CPU')
     ).toFixed(2);
+}
+
+function plotarGraficoLinha() {
+    var options = {
+        series: [
+            {
+                name: 'Uso',
+                data: dados_uso,
+            },
+        ],
+        chart: {
+            height: 400,
+            type: 'line',
+            zoom: {
+                enabled: false,
+            },
+        },
+        dataLabels: {
+            enabled: false,
+        },
+        stroke: {
+            curve: 'straight',
+            width: `${vwParaPx(0.2)}`,
+        },
+        title: {
+            align: 'left',
+        },
+        xaxis: {
+            categories: horarios_uso,
+        },
+        legend: {
+            position: 'top',
+            fontSize: `${vwParaPx(1)}px`,
+            fontFamily: 'poppins leve',
+            markers: {
+                size: `${vwParaPx(0.4)}`,
+            },
+        },
+        toolbar: {
+            show: true,
+            tools: {
+                download: true,
+                selection: false,
+                zoom: false,
+                zoomin: false,
+                zoomout: false,
+                pan: false,
+                reset: false,
+            },
+            offsetX: 0,
+            offsetY: 0,
+        },
+        colors: ['#268184'],
+        yaxis: {
+            min: 0,
+            max: 100,
+            labels: {
+                style: {
+                    fontSize: `${vwParaPx(0.8)}px`,
+                    fontFamily: 'poppins leve',
+                },
+            },
+        },
+        annotations: {
+            yaxis: [
+                {
+                    y: parseFloat(sessionStorage.getItem('PARAM_CRITICO_CPU')),
+                    borderColor: '#FF4560',
+                    strokeDashArray: 0,
+                    label: {
+                        borderColor: '#FF4560',
+                        style: {
+                            color: '#fff',
+                            background: '#FF4560',
+                        },
+                        text: 'Crítico',
+                    },
+                },
+                {
+                    y: parseFloat(sessionStorage.getItem('PARAM_IMPORTANTE_CPU')),
+                    borderColor: '#f4a261',
+                    strokeDashArray: 0,
+                    label: {
+                        borderColor: '#f4a261',
+                        style: {
+                            color: '#fff',
+                            background: '#f4a261',
+                        },
+                        text: 'Importante',
+                    },
+                },
+            ],
+        },
+    };
+
+    var chart = new ApexCharts(document.querySelector('#grafico-linha'), options);
+    chart.render();
 }
 
 window.addEventListener('DOMContentLoaded', () => {
