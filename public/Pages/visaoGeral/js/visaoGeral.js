cff97a0003360683bf8133d22b5aee6710b5e86d
brenokas <brenofreitas28f@gmail.com>
2025-11-25 00:05:27 -0300

    add: inicio de padronizacao da navbar

--- public/Pages/visaoGeral/js/visaoGeral.js
+++ public/Pages/visaoGeral/js/visaoGeral.js
@@ -236,27 +236,6 @@ function carregarAtms() {
         });
 }
 
-function abrirDashboards() {
-    let botoesIndividuais = document.getElementById('botoes-ind');
-    let setaDireita = document.getElementById('seta-dashboards');
-
-    if (setaDireita.classList.contains('botao-seta-baixo')) {
-        setaDireita.classList.remove('botao-seta-baixo');
-        setaDireita.classList.add('botao-seta-direita');
-    } else {
-        setaDireita.classList.remove('botao-seta-direita');
-        setaDireita.classList.add('botao-seta-baixo');
-    }
-
-    if (botoesIndividuais.classList.contains('botoes-individuais-inativo')) {
-        botoesIndividuais.classList.add('botoes-individuais-ativo');
-        botoesIndividuais.classList.remove('botoes-individuais-inativo');
-    } else {
-        botoesIndividuais.classList.remove('botoes-individuais-ativo');
-        botoesIndividuais.classList.add('botoes-individuais-inativo');
-    }
-}
-
 window.addEventListener('DOMContentLoaded', () => {
     carregarParametros();
     carregarAtms();
