df9c17680552e893794f3a723269fe6c0b8b61ad
brenokas <brenofreitas28f@gmail.com>
2025-11-24 23:39:54 -0300

    add: botoes das dashboard individuais na barra lateral da visao geral e cores na tabela de atms da visao geral

--- public/Pages/visaoGeral/js/visaoGeral.js
+++ public/Pages/visaoGeral/js/visaoGeral.js
@@ -10,23 +10,41 @@ function carregarParametros() {
                             switch (resposta[i]['fkTipoComponente']) {
                                 case 1:
                                     if (resposta[i]['fkTipoAlerta'] == 1) {
-                                        sessionStorage.setItem('PARAM_CRITICO_CPU', resposta[i]['limiteMax']);
+                                        sessionStorage.setItem(
+                                            'PARAM_CRITICO_CPU',
+                                            resposta[i]['limiteMax']
+                                        );
                                     } else {
-                                        sessionStorage.setItem('PARAM_IMPORTANTE_CPU', resposta[i]['limiteMax']);
+                                        sessionStorage.setItem(
+                                            'PARAM_IMPORTANTE_CPU',
+                                            resposta[i]['limiteMax']
+                                        );
                                     }
                                     break;
                                 case 2:
                                     if (resposta[i]['fkTipoAlerta'] == 1) {
-                                        sessionStorage.setItem('PARAM_CRITICO_RAM', resposta[i]['limiteMax']);
+                                        sessionStorage.setItem(
+                                            'PARAM_CRITICO_RAM',
+                                            resposta[i]['limiteMax']
+                                        );
                                     } else {
-                                        sessionStorage.setItem('PARAM_IMPORTANTE_RAM', resposta[i]['limiteMax']);
+                                        sessionStorage.setItem(
+                                            'PARAM_IMPORTANTE_RAM',
+                                            resposta[i]['limiteMax']
+                                        );
                                     }
                                     break;
                                 case 3:
                                     if (resposta[i]['fkTipoAlerta'] == 1) {
-                                        sessionStorage.setItem('PARAM_CRITICO_DISCO', resposta[i]['limiteMax']);
+                                        sessionStorage.setItem(
+                                            'PARAM_CRITICO_DISCO',
+                                            resposta[i]['limiteMax']
+                                        );
                                     } else {
-                                        sessionStorage.setItem('PARAM_IMPORTANTE_DISCO', resposta[i]['limiteMax']);
+                                        sessionStorage.setItem(
+                                            'PARAM_IMPORTANTE_DISCO',
+                                            resposta[i]['limiteMax']
+                                        );
                                     }
                                     break;
                                 case 5:
@@ -67,7 +85,6 @@ function carregarAtms() {
             if (response.ok) {
                 response.json().then(resposta => {
                     if (resposta.length > 0) {
-                        console.log(resposta);
                         let tabelaBody = document.getElementById('corpo-tabela-visao-geral');
                         let statusMonitoramentoAtm = 'Normal';
                         for (let i = 0; i < resposta.length; i++) {
@@ -89,7 +106,9 @@ function carregarAtms() {
                                 `;
                                 atmsCarregados.push(resposta[i].numeracao);
                             } else {
-                                document.getElementById(`linha-atm${resposta[i].numeracao}`).innerHTML = `
+                                document.getElementById(
+                                    `linha-atm${resposta[i].numeracao}`
+                                ).innerHTML = `
                                 <th class="celula-tabela" scope="row">${resposta[i].numeracao}</th>
                                         <td class="celula-tabela" id="uso-cpu-atm-${resposta[i].numeracao}">${resposta[i].uso_cpu}%</td>
                                         <td class="celula-tabela" id="uso-ram-atm-${resposta[i].numeracao}">${resposta[i].uso_ram}%</td>
@@ -99,83 +118,145 @@ function carregarAtms() {
                                 `;
                             }
 
-                            if (resposta[i].uso_cpu >= sessionStorage.getItem('PARAM_CRITICO_CPU')) {
-                                document.getElementById(`uso-cpu-atm-${resposta[i].numeracao}`).style.color = '#e63946';
-                                document.getElementById(`uso-cpu-atm-${resposta[i].numeracao}`).style.fontWeight =
-                                    'bold';
-                                document.getElementById(`status-monitoramento-atm-${resposta[i].numeracao}`).innerText =
-                                    'Crítico';
+                            if (
+                                resposta[i].uso_cpu >= sessionStorage.getItem('PARAM_CRITICO_CPU')
+                            ) {
+                                document.getElementById(
+                                    `uso-cpu-atm-${resposta[i].numeracao}`
+                                ).style.color = '#e63946';
+                                document.getElementById(
+                                    `uso-cpu-atm-${resposta[i].numeracao}`
+                                ).style.fontWeight = 'bold';
+                                document.getElementById(
+                                    `status-monitoramento-atm-${resposta[i].numeracao}`
+                                ).innerText = 'Crítico';
                                 document
-                                    .getElementById(`status-monitoramento-atm-${resposta[i].numeracao}`)
+                                    .getElementById(
+                                        `status-monitoramento-atm-${resposta[i].numeracao}`
+                                    )
                                     .classList.remove('celula-normal');
                                 document
-                                    .getElementById(`status-monitoramento-atm-${resposta[i].numeracao}`)
+                                    .getElementById(
+                                        `status-monitoramento-atm-${resposta[i].numeracao}`
+                                    )
                                     .classList.add('celula-critico');
-                            } else if (resposta[i].uso_cpu >= sessionStorage.getItem('PARAM_IMPORTANTE_CPU')) {
-                                document.getElementById(`uso-cpu-atm-${resposta[i].numeracao}`).style.color = '#f4a261';
-                                document.getElementById(`uso-cpu-atm-${resposta[i].numeracao}`).style.fontWeight =
-                                    'bold';
-                                document.getElementById(`status-monitoramento-atm-${resposta[i].numeracao}`).innerText =
-                                    'Importante';
+                            } else if (
+                                resposta[i].uso_cpu >=
+                                sessionStorage.getItem('PARAM_IMPORTANTE_CPU')
+                            ) {
+                                document.getElementById(
+                                    `uso-cpu-atm-${resposta[i].numeracao}`
+                                ).style.color = '#f4a261';
+                                document.getElementById(
+                                    `uso-cpu-atm-${resposta[i].numeracao}`
+                                ).style.fontWeight = 'bold';
+                                document.getElementById(
+                                    `status-monitoramento-atm-${resposta[i].numeracao}`
+                                ).innerText = 'Importante';
                                 document
-                                    .getElementById(`status-monitoramento-atm-${resposta[i].numeracao}`)
+                                    .getElementById(
+                                        `status-monitoramento-atm-${resposta[i].numeracao}`
+                                    )
                                     .classList.remove('celula-normal');
                                 document
-                                    .getElementById(`status-monitoramento-atm-${resposta[i].numeracao}`)
+                                    .getElementById(
+                                        `status-monitoramento-atm-${resposta[i].numeracao}`
+                                    )
                                     .classList.add('celula-importante');
                             }
 
-                            if (resposta[i].uso_ram >= sessionStorage.getItem('PARAM_CRITICO_RAM')) {
-                                document.getElementById(`uso-ram-atm-${resposta[i].numeracao}`).style.color = '#e63946';
-                                document.getElementById(`uso-ram-atm-${resposta[i].numeracao}`).style.fontWeight =
-                                    'bold';
-                                document.getElementById(`status-monitoramento-atm-${resposta[i].numeracao}`).innerText =
-                                    'Crítico';
+                            if (
+                                resposta[i].uso_ram >= sessionStorage.getItem('PARAM_CRITICO_RAM')
+                            ) {
+                                document.getElementById(
+                                    `uso-ram-atm-${resposta[i].numeracao}`
+                                ).style.color = '#e63946';
+                                document.getElementById(
+                                    `uso-ram-atm-${resposta[i].numeracao}`
+                                ).style.fontWeight = 'bold';
+                                document.getElementById(
+                                    `status-monitoramento-atm-${resposta[i].numeracao}`
+                                ).innerText = 'Crítico';
                                 document
-                                    .getElementById(`status-monitoramento-atm-${resposta[i].numeracao}`)
+                                    .getElementById(
+                                        `status-monitoramento-atm-${resposta[i].numeracao}`
+                                    )
                                     .classList.remove('celula-normal');
                                 document
-                                    .getElementById(`status-monitoramento-atm-${resposta[i].numeracao}`)
+                                    .getElementById(
+                                        `status-monitoramento-atm-${resposta[i].numeracao}`
+                                    )
                                     .classList.add('celula-critico');
-                            } else if (resposta[i].uso_ram >= sessionStorage.getItem('PARAM_IMPORTANTE_RAM')) {
-                                document.getElementById(`uso-ram-atm-${resposta[i].numeracao}`).style.color = '#f4a261';
-                                document.getElementById(`uso-ram-atm-${resposta[i].numeracao}`).style.fontWeight =
-                                    'bold';
-                                document.getElementById(`status-monitoramento-atm-${resposta[i].numeracao}`).innerText =
-                                    'Importante';
+                            } else if (
+                                resposta[i].uso_ram >=
+                                sessionStorage.getItem('PARAM_IMPORTANTE_RAM')
+                            ) {
+                                document.getElementById(
+                                    `uso-ram-atm-${resposta[i].numeracao}`
+                                ).style.color = '#f4a261';
+                                document.getElementById(
+                                    `uso-ram-atm-${resposta[i].numeracao}`
+                                ).style.fontWeight = 'bold';
+                                document.getElementById(
+                                    `status-monitoramento-atm-${resposta[i].numeracao}`
+                                ).innerText = 'Importante';
                                 document
-                                    .getElementById(`status-monitoramento-atm-${resposta[i].numeracao}`)
+                                    .getElementById(
+                                        `status-monitoramento-atm-${resposta[i].numeracao}`
+                                    )
                                     .classList.remove('celula-normal');
                                 document
-                                    .getElementById(`status-monitoramento-atm-${resposta[i].numeracao}`)
+                                    .getElementById(
+                                        `status-monitoramento-atm-${resposta[i].numeracao}`
+                                    )
                                     .classList.add('celula-importante');
                             }
 
-                            if (resposta[i].uso_disco >= sessionStorage.getItem('PARAM_CRITICO_DISCO')) {
-                                document.getElementById(`uso-disco-atm-${resposta[i].numeracao}`).style.color =
-                                    '#e63946';
-                                document.getElementById(`uso-disco-atm-${resposta[i].numeracao}`).style.fontWeight =
-                                    'bold';
-                                document.getElementById(`status-monitoramento-atm-${resposta[i].numeracao}`).innerText =
-                                    'Crítico';
+                            if (
+                                resposta[i].uso_disco >=
+                                sessionStorage.getItem('PARAM_CRITICO_DISCO')
+                            ) {
+                                document.getElementById(
+                                    `uso-disco-atm-${resposta[i].numeracao}`
+                                ).style.color = '#e63946';
+                                document.getElementById(
+                                    `uso-disco-atm-${resposta[i].numeracao}`
+                                ).style.fontWeight = 'bold';
+                                document.getElementById(
+                                    `status-monitoramento-atm-${resposta[i].numeracao}`
+                                ).innerText = 'Crítico';
                                 document
-                                    .getElementById(`status-monitoramento-atm-${resposta[i].numeracao}`)
+                                    .getElementById(
+                                        `status-monitoramento-atm-${resposta[i].numeracao}`
+                                    )
                                     .classList.remove('celula-normal');
                                 document
-                                    .getElementById(`status-monitoramento-atm-${resposta[i].numeracao}`)
+                                    .getElementById(
+                                        `status-monitoramento-atm-${resposta[i].numeracao}`
+                                    )
                                     .classList.add('celula-critico');
-                            } else if (resposta[i].uso_disco >= sessionStorage.getItem('PARAM_IMPORTANTE_DISCO')) {
-                                document.getElementById(`uso-disco-atm-${resposta[i].numeracao}`).style.color =
-                                    '#f4a261';
-                                document.getElementById(`uso-disco-atm-${resposta[i].numeracao}`).style.fontWeight =
-                                    'bold';
-                                document.getElementById(`status-monitoramento-atm-${resposta[i].numeracao}`).innerText =
-                                    'Importante';
+                            } else if (
+                                resposta[i].uso_disco >=
+                                sessionStorage.getItem('PARAM_IMPORTANTE_DISCO')
+                            ) {
+                                document.getElementById(
+                                    `uso-disco-atm-${resposta[i].numeracao}`
+                                ).style.color = '#f4a261';
+                                document.getElementById(
+                                    `uso-disco-atm-${resposta[i].numeracao}`
+                                ).style.fontWeight = 'bold';
+                                document.getElementById(
+                                    `status-monitoramento-atm-${resposta[i].numeracao}`
+                                ).innerText = 'Importante';
                                 document
-                                    .getElementById(`status-monitoramento-atm-${resposta[i].numeracao}`)
+                                    .getElementById(
+                                        `status-monitoramento-atm-${resposta[i].numeracao}`
+                                    )
                                     .classList.remove('celula-normal');
                                 document
-                                    .getElementById(`status-monitoramento-atm-${resposta[i].numeracao}`)
+                                    .getElementById(
+                                        `status-monitoramento-atm-${resposta[i].numeracao}`
+                                    )
                                     .classList.add('celula-importante');
                             }
                         }
@@ -190,6 +271,27 @@ function carregarAtms() {
         .catch(erro => {
             console.error(`carregarAtms: erro na obtenção dos dados: ${erro.message}`);
         });
+}
+
+function abrirDashboards() {
+    let botoesIndividuais = document.getElementById('botoes-ind');
+    let setaDireita = document.getElementById('seta-dashboards');
+
+    if (setaDireita.classList.contains('botao-seta-baixo')) {
+        setaDireita.classList.remove('botao-seta-baixo');
+        setaDireita.classList.add('botao-seta-direita');
+    } else {
+        setaDireita.classList.remove('botao-seta-direita');
+        setaDireita.classList.add('botao-seta-baixo');
+    }
+
+    if (botoesIndividuais.classList.contains('botoes-individuais-inativo')) {
+        botoesIndividuais.classList.add('botoes-individuais-ativo');
+        botoesIndividuais.classList.remove('botoes-individuais-inativo');
+    } else {
+        botoesIndividuais.classList.remove('botoes-individuais-ativo');
+        botoesIndividuais.classList.add('botoes-individuais-inativo');
+    }
 }
 
 window.addEventListener('DOMContentLoaded', () => {
