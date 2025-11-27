57758b5bde98f147174a78077553abae3aac843b
PereiragGabriel <gabriel.gpereira@sptech.school>
2025-11-27 17:19:51 -0300

    adiciona a logica ccorreta de orquestração no controller

--- src/controllers/ramController.js
+++ src/controllers/ramController.js
@@ -34,12 +34,12 @@ function ObterKPI_2(req, res) {
 
 function ObterKPI_3(req, res) {
     let idEmpresa = req.params.idEmpresa;
-
+    console.log("CHEGEUI AQUI MANO=========================================")
     ramModel.ObterKPI_3(idEmpresa)
         .then((resultado) => {
             console.log("resultado da controller:", resultado);
             res.json({
-                qtdHorario: resultado[0].qtdHorario
+                qtdHorario: resultado[0].hora_do_dia?resultado[0].hora_do_dia:null
             })
         }).catch((erro) => {
             console.log(erro);
@@ -55,7 +55,7 @@ function CarregarDadosGraficoUsoAtual(req, res) {
         .then((resultado) => {
             console.log("resultado da controller:", resultado);
             res.json({
-                grafUsoAtual: resultado[0].grafUsoAtual
+                grafUsoAtual: resultado
             })
         }).catch((erro) => {
             console.log(erro);
@@ -64,18 +64,18 @@ function CarregarDadosGraficoUsoAtual(req, res) {
         });
 }
 
-function  CarregarDadosGraficoDeUso(req, res) {
+function CarregarDadosGraficoDeUso(req, res) {
     let idEmpresa = req.params.idEmpresa;
-
+grafUsoAtual
     ramModel.CarregarDadosGraficoDeUso(idEmpresa)
         .then((resultado) => {
             console.log("resultado da controller:", resultado);
             res.json({
-                grafMaiorHr: resultado[0].grafMaiorHr
+                usoAtualRam: resultado[0].statusUso 
             })
         }).catch((erro) => {
             console.log(erro);
-            console.log("Erro ao mudar status do alerta:", erro.sqlMessage);
+            console.log("Erro ao carregar dados do gráfico de uso atual:", erro.sqlMessage);
             res.status(500).json(erro.sqlMessage);
         });
 }
